91a44082ecda3a3570a56df514130fa6e77ce935
TIAGO SILVA <frost.tiago@gmail.com>
2020-10-06 12:37:12 -0300

    Add exercícios

---
+++ Aula07_IntroducaoOrientacaoObjetos/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula07_IntroducaoOrientacaoObjetos
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+        public double Peso;
+        public double Altura;
+
+        public void Estudar()
+        {
+            Console.WriteLine("{0} Está estudando.", Nome);
+        }
+        public void Dormir()
+        {
+            Console.WriteLine("{0} Está dormindo.", Nome);
+        }
+
+        public void Andar()
+        {
+            Console.WriteLine("{0} Está andando.", Nome);
+        }
+    }
+}
--- Aula07_IntroducaoOrientacaoObjetos/Program.cs
+++ Aula07_IntroducaoOrientacaoObjetos/Program.cs
@@ -16,11 +16,37 @@ namespace Aula07_IntroducaoOrientacaoObjetos
             rex.Correr();
             rex.Morder();
 
+            Console.WriteLine("__________________________________");
 
             Cachorro milu = new Cachorro();
             milu.Nome = "Milu do Tintin";
             milu.Raca = "Fox Terrier";
             milu.Correr();
+
+            Console.WriteLine("__________________________________");
+
+
+            /**
+             * Faça uma classe e dessa classe crie 3 objetos para mapear características e
+             * comportamentos de uma pessoa.
+             */
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Henrique";
+            p1.Estudar();
+            p1.Andar();
+
+            Pessoa p2 = new Pessoa();
+            p2.Nome = "Styven";
+            p2.Estudar();
+
+            Pessoa p3 = new Pessoa();
+            p3.Nome = "Ygor";
+            p3.Estudar();
+
+            /**
+             * Faça uma classe e dessa classe crie 3 objetos para mapear características e
+             * comportamentos de um gato. A clase deverá ter 4 métodos e 4 Propriedades.
+             */
         }
     }
 }
