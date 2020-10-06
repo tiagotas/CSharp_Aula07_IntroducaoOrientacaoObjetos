998341d79036b49e0746149aeaa5553faeb7196c
TIAGO SILVA <frost.tiago@gmail.com>
2020-10-06 12:04:04 -0300

    Adicionar arquivos de projeto.

---
+++ Aula07_IntroducaoOrientacaoObjetos/Cachorro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula07_IntroducaoOrientacaoObjetos
+{
+    public class Cachorro
+    {
+        // Propriedades da classe
+        public int Patas;
+        public string Focinho;
+        public string Pelo;
+        public string Nome;
+        public string Raca;
+
+
+        //Métodos (comportamentos/atividades) da classe
+
+        public void Latir()
+        {
+            Console.WriteLine("O cachorro {0} da raça {1} está latindo.", Nome, Raca);
+        }
+
+        public void Morder()
+        {
+            Console.WriteLine("O cachorro {0} da raça {1} está mordendo.", Nome, Raca);
+        }
+
+        public void Dormir()
+        {
+            Console.WriteLine("O cachorro {0} da raça {1} está domindo.", Nome, Raca);
+        }
+
+        public void Correr()
+        {
+            Console.WriteLine("O cachorro {0} da raça {1} está correndo.", Nome, Raca);
+        }
+    }
+}
---
+++ Aula07_IntroducaoOrientacaoObjetos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Aula07_IntroducaoOrientacaoObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando um novo objeto a partir da classe Cachorro
+            // O novo objeto "está dentro" da variável rex.
+            Cachorro rex = new Cachorro();
+            rex.Nome = "Rex da Silva";
+            rex.Raca = "Pinscher";
+            rex.Latir();
+            rex.Correr();
+            rex.Morder();
+
+
+            Cachorro milu = new Cachorro();
+            milu.Nome = "Milu do Tintin";
+            milu.Raca = "Fox Terrier";
+            milu.Correr();
+        }
+    }
+}
